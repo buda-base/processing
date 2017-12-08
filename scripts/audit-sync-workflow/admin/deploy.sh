19a01abb48dd504ca71ac1563d14ae88acfee441
BDRC-Travis <travis@tbrc.org>
2017-12-08 11:24:24 -0500

    adding deploy script to setup initial dirs and update scripts from git

--- scripts/audit-sync-workflow/admin/deploy.sh
+++ scripts/audit-sync-workflow/admin/deploy.sh
@@ -2,6 +2,34 @@
 
 workflowDir="/Volumes/staging"
 
-rsync ../audit/ $workflowDir/audit/scripts
-rsync ../fix/ $workflowDir/fix/scripts
-rsync ../sync/ $workflowDir/sync/scripts
+while [ ! $# -eq 0 ]; do
+
+  case "$1" in
+
+    --init | -i)
+
+      mkdir -p $workflowDir/audit/logs
+      mkdir -p $workflowDir/audit/scripts
+      mkdir -p $workflowDir/audit/works
+      mkdir -p $workflowDir/sync/logs
+      mkdir -p $workflowDir/sync/scripts
+      mkdir -p $workflowDir/sync/works
+      mkdir -p $workflowDir/done
+
+      exit
+      ;;
+
+    --update | -u)
+      
+      rsync -av ../audit/ $workflowDir/audit/scripts
+      #rsync -av ../fix/ $workflowDir/fix/scripts
+      rsync -av ../sync/ $workflowDir/sync/scripts
+
+      exit
+      ;;
+
+  esac
+
+  shift
+done
+
