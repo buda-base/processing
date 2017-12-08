4cb7a856b295fbf344e32ee316db80e73fb14ee7
BDRC-Travis <travis@tbrc.org>
2017-12-08 16:09:19 -0500

    update audit checks 1 and 2 to be more user friendly

--- scripts/audit-sync-workflow/audit/audit_check.sh
+++ scripts/audit-sync-workflow/audit/audit_check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dateTimeStamp=`date +'%Y%m%d_%H%M%S'`
-auditLog="/Volumes/staging/audit/logs/audit_log_${dateTimeStamp}.txt"
+auditLog="/Users/tbrc/staging/processing/scripts/audit-sync-workflow/test/audit/logs/audit_log_${dateTimeStamp}.txt"
 
 echo "" | tee -a $auditLog
 echo "*****************************************************************" | tee -a $auditLog
@@ -18,16 +18,28 @@ for w in `ls -d W*`; do
   for d in `ls`; do
     cd $d
 
-    archive_num=`ls ../../archive/$d/*.* | wc -l`
-    images_num=`ls *.* | wc -l`
+    if [ -d "../../archive/$d" ]; then
+      archive_num=`ls ../../archive/$d/*.* | wc -l`
+      images_num=`ls *.* | wc -l`
 
-    if [ $archive_num -ne $images_num ]; then
+      if [ $archive_num -ne $images_num ]; then
+        auditCheck1Failed=true
+
+        echo "" | tee -a $auditLog
+        echo "------ DISCREPANCY FOUND ------" | tee -a $auditLog
+        echo "- $d" | tee -a $auditLog
+        echo "- a: $archive_num" | tee -a $auditLog
+        echo "- i: $images_num" | tee -a $auditLog
+        echo "-------------------------------" | tee -a $auditLog
+      fi
+    else
       auditCheck1Failed=true
 
-      echo "-- DISCREPANCY FOUND --" | tee -a $auditLog
-      echo "$d" | tee -a $auditLog
-      echo "a: $archive_num" | tee -a $auditLog
-      echo "i: $images_num" | tee -a $auditLog
+      echo "" | tee -a $auditLog
+      echo "------ DISCREPANCY FOUND ------" | tee -a $auditLog
+      echo "- Cannot perform comparison." | tee -a $auditLog
+      echo "- The folder $w/archive/$d does not exist." | tee -a $auditLog
+      echo "-------------------------------" | tee -a $auditLog
     fi
 
     cd ..
@@ -39,9 +51,9 @@ done
 echo "" | tee -a $auditLog
 
 if $auditCheck1Failed; then
-  echo "AUDIT CHECK #1 STATUS -- FAILED" | tee -a $auditLog
+  echo "=== AUDIT CHECK #1 STATUS -- FAILED ===" | tee -a $auditLog
 else
-  echo "AUDIT CHECK #1 STATUS -- PASSED" | tee -a $auditLog
+  echo "=== AUDIT CHECK #1 STATUS -- PASSED ===" | tee -a $auditLog
 fi
 
 echo "" | tee -a $auditLog
@@ -62,10 +74,25 @@ for w in `ls -d W*`; do
     for i in `ls`; do
       cd $i
       imgGrp=`echo $i | cut -d'-' -f2`
-      echo "$w:" | tee -a $auditLog
-      for f in `find . ! -name "$imgGrp*"`; do
-        echo $f | tee -a $auditLog
-      done
+
+      if [[ -n $(find . ! -name "$imgGrp*" -type f) ]]; then
+        auditCheck2Failed=true
+
+        echo ""
+        echo "------ DISCREPANCY FOUND ------" | tee -a $auditLog
+        echo "-" | tee -a $auditLog
+        echo "- INCORRECT FILENAMES FOUND" | tee -a $auditLog
+        echo "-" | tee -a $auditLog
+        echo "- $w-$imgGrp:" | tee -a $auditLog
+
+        for f in `find . ! -name "$imgGrp*" -type f`; do
+          echo "- $f" | tee -a $auditLog
+        done
+        echo "-" | tee -a $auditLog
+        echo "-------------------------------" | tee -a $auditLog
+
+      fi
+
       cd ..
     done
     cd ..
@@ -73,6 +100,13 @@ for w in `ls -d W*`; do
   cd ..
 done
 
+echo "" | tee -a $auditLog
+
+if $auditCheck2Failed; then
+  echo "=== AUDIT CHECK #2 STATUS -- FAILED ===" | tee -a $auditLog
+else
+  echo "=== AUDIT CHECK #2 STATUS -- PASSED ===" | tee -a $auditLog
+fi
 
 #echo "" | tee -a $auditLog
 #echo "*****************************************************************" | tee -a $auditLog
@@ -103,7 +137,7 @@ done
 
 echo "" | tee -a $auditLog
 echo "*****************************************************************" | tee -a $auditLog
-echo "* #4 - CHECK FOR FILES > 400K in IMAGES FOLDER" | tee -a $auditLog
+echo "* #3 - CHECK FOR FILES > 400K in IMAGES FOLDER" | tee -a $auditLog
 echo "*****************************************************************" | tee -a $auditLog
 echo "" | tee -a $auditLog
 
